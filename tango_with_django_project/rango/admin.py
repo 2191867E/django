4ba369e091d3d2d364c993c923e6978e8d2ae3e8
Jason Egan <2191867E@student.gla.ac.uk>
2017-01-26 11:29:23 +0000

    chapter 5 progress

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from rango.models import Category,Page
+from rango.models import Question
+
 
 admin.site.register(Category)
 admin.site.register(Page)
+admin.site.register(Question)

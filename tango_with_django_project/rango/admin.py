59ec365d81a18aaba1864fd9d5a84d997c377ff4
Jason Egan <2191867E@student.gla.ac.uk>
2017-01-26 11:55:35 +0000

    changes

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from rango.models import Category,Page
-from rango.models import Question
+from rango.models import Question,Choice
 
 
 admin.site.register(Category)
 admin.site.register(Page)
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        (None,              {'fields':['question_text']}),
+        ('Date Information',{'fields':['pub_date'],'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text','pub_date','was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question,QuestionAdmin)

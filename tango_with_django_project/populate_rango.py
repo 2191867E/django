b65aca61f0f6a11d88631e6dfd9ab5b3b236547b
Jason <2191867E@student.gla.ac.uk>
2017-02-02 17:30:13 +0000

    chapter 8 finished

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -8,28 +8,28 @@ from rango.models import Category,Page
 def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url':'http://docs.python.org/2/tutorial/'},
+         'url':'http://docs.python.org/2/tutorial/','views':128,'likes':64},
         {'title': 'How to Think like a Computer Scientist',
-         'url':'http://www.greenteapress.com/thinkpython/'},
+         'url':'http://www.greenteapress.com/thinkpython/','views':256,'likes':32},
         {'title':'Learn Python in 10 Minutes',
-         'url':'http://www.korokithakis.net/tutorials/python/'}
+         'url':'http://www.korokithakis.net/tutorials/python/','views':128,'likes':0}
     ]
 
     django_pages = [
         {'title':'Official Django Tutorial',
-         'url':'http://docs.djangoproject.com/en/1.9/intro/tutorial01/'},
+         'url':'http://docs.djangoproject.com/en/1.9/intro/tutorial01/','views':256,'likes':100},
         {'title':'Django Rocks',
-         'url':'http://www.djangorocks.com/'},
+         'url':'http://www.djangorocks.com/','views':90,'likes':28},
         {'title':'How to Tango with Django',
-        'url':'http://www.tangowithdjango.com/'}
+        'url':'http://www.tangowithdjango.com/','views':512,'likes':256}
 
     ]
 
     other_pages = [
         {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/'},
+         'url':'http://bottlepy.org/docs/dev/','views':28,'likes':56},
         {'title':'Flask',
-         'url':'http://flask.pocoo.org'
+         'url':'http://flask.pocoo.org','views':100,'likes':28
         }
 
     ]
@@ -47,7 +47,7 @@ def populate():
         for p in Page.objects.filter(category = c):
             print("- {0} - {1}".format(str(c),str(p)))
 
-def add_page(cat,title,url,view = 0):
+def add_page(cat,title,url):
     p = Page.objects.get_or_create(category=cat,title=title)[0]
     p.url = url
     p.save()
@@ -55,15 +55,6 @@ def add_page(cat,title,url,view = 0):
 
 def add_cat(name):
     c = Category.objects.get_or_create(name=name)[0]
-    if name == 'Python':
-        c.view = 128
-        c.likes = 64
-    elif name == 'Django':
-        c.view = 64
-        c.likes = 32
-    else:
-        c.view = 32
-        c.likes = 16
     c.save()
     return c
 
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -29,7 +29,7 @@ class Choice(models.Model):
 
 class Category(models.Model):
 	name = models.CharField(max_length = 128, unique = True)
-	view = models.IntegerField(default = 0)
+	views = models.IntegerField(default = 0)
 	likes = models.IntegerField(default = 0)
 	slug = models.SlugField(unique = True)
 	
